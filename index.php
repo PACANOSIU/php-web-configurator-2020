204514f47173d739f7de13ebe244f8e89c3ef339
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-24 21:17:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,137 @@
+<?php
+if($_GET)
+{
+	$_config="https://iepa.pl/copy/config.php";
+	$_new_config="config.php";
+	copy($_config,$_new_config);
+	$_konfigurator_pikiow=fopen(".htaccess","w")or die("Unable to open file!");	
+	$_zawartosc_przesylu_htaccess="Options FollowSymLinks
+	RewriteEngine On
+	RewriteRule ^([0-9a-zA-Z\-\#]+)/?$ index.php?page=$1 [L]
+	RewriteCond %{HTTPS} !=on
+	php_value date.timezone 'Europe/Warsaw'";
+	fwrite($_konfigurator_pikiow,$_zawartosc_przesylu_htaccess);
+	fclose($_konfigurator_pikiow);
+	
+$_POST='$_POST';
+$_host_bazy_danych='$_POST["host_bazy_danych"]';
+$_post_nazwa_uzytkownik_bazy_danych='$_POST["nazwa_uzytkownika_bazy_danych"]';
+$_post_haslo_uzytkownika_bazy_danych='$_POST["haslo_uzytkownika_bazy_danych"]';
+$_post_nazwa_bazy_danych='$_POST["nazwa_bazy_danych"]';
+$_post_konfigurator='$_POST["konfigurator"]';
+	
+$_zawartosc_przesylu=
+	'<!DOCTYPE html>
+		<html>
+			<head>
+				<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+			</head>
+			<body>
+				<div style="text-align:center;font-family:Montserrat,sans-serif;">
+					<form method="POST">
+						<h1>
+							Połączenie z bazą danych
+						</h1>
+						<div style="margin:10px;">
+							Wprowadź nazwę użytkownika bazy danych
+							<br />
+							przykładowo 
+							<i>
+								root
+							</i>
+							<br />
+							<input style="width:300px;margin:10px;" type="text" name="nazwa_uzytkownika_bazy_danych">
+						</div>
+						<div style="margin:10px;">
+							Wprowadź hasło użytkownika bazy danych
+							<br />
+							(gdy brak nic nie wpisuj, zostaw puste pole)
+							<br />
+							<input style="width:300px;margin:10px;" type="password" name="haslo_uzytkownika_bazy_danych">
+						</div>
+						<div style="margin:10px;">
+							Wprowadź nazwę bazy danych
+							<br />
+							<input style="width:300px;margin:10px;" type="text" name="nazwa_bazy_danych">
+						</div>
+						<div style="margin:10px;">
+							Wprowadź host bazy danych
+							<br />
+							<input style="width:300px;margin:10px;" type="text" name="host_bazy_danych">
+						</div>
+						<div align="center">
+							<button style="display:block;font-family:Montserrat,sans-serif;font-size:20px;padding:10px;margin:10px;cursor:pointer;" type="submit" name="konfigurator" value="pzbdikd">
+								POŁĄCZ Z BAZĄ DANYCH I KONFIGURUJ DALEJ
+							</button>
+							<button style="font-family:Montserrat,sans-serif;font-size:20px;padding:10px;margin:10px;cursor:pointer;" type="submit" name="konfigurator" value="pikd">
+								POMIŃ I KONFIGURUJ DALEJ
+							</button>
+						</div>
+					</form>
+				</div>	
+			</body>
+			<footer style="position:absolute;bottom:1px;right:1px;font-size:10px;font-family:Montserrat,sans-serif;">
+				Konfigurator PACANOSIU BETA 2020.01
+			</footer>
+		</html>'
+;
+$_konfigurator_pikiow=fopen("konfigurator.php","w")or die("Unable to open file!");
+$_zawartosc_przesylu="<?php
+	if($_POST)
+	{		
+		if($_post_konfigurator=='pzbdikd')
+		{
+			echo$_post_nazwa_uzytkownik_bazy_danych;
+			echo'<br />';
+			echo$_post_haslo_uzytkownika_bazy_danych;
+			echo'<br />';
+			echo$_post_nazwa_bazy_danych;
+			echo'<br />';
+			echo$_post_konfigurator;
+		}
+		elseif($_post_konfigurator=='pikd')
+		{
+			echo$_post_konfigurator;
+			echo'<br />';
+			echo'KONFIGURACJA BEZ POŁACZENIA Z BAZĄ DANYCH';
+		}
+		$_KONFIGURATOR_CD_1;
+		
+		
+	}
+	echo
+		'".$_zawartosc_przesylu."'
+	;
+?>";
+fwrite($_konfigurator_pikiow,$_zawartosc_przesylu);
+fclose($_konfigurator_pikiow);
+header("location:konfigurator.php");
+}
+	
+	echo
+		'<!DOCTYPE html>
+		<html>
+			<head>
+				<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+			</head>
+			<body>
+				<div style="text-align:center;font-family:Montserrat,sans-serif;">
+					<div>
+						Witaj w profesjonalnym konfiguratorze stron internetowych "PACANOSIU"
+						<br />
+						Dziękuję za zinteresowanie moim produktem.
+						<br />
+						Aby być na bierząco <a href="">SUBSKRYBUJ</a>.
+						<br />
+						<form method="GET">
+							<input style="font-family:Montserrat,sans-serif;font-size:20px;padding:10px;margin:10px;cursor:pointer;" type="submit" name="konfigurator" value="ROZPOCZNIJ KONFIGURACJĘ SWOJEJ STRONY">
+						</form>
+					</div>
+				</div>
+			</body>
+			<footer style="position:absolute;bottom:1px;right:1px;font-size:10px;font-family:Montserrat,sans-serif;">
+				Konfigurator PACANOSIU BETA 2020.01
+			</footer>
+		</html>'
+	;
+?>
