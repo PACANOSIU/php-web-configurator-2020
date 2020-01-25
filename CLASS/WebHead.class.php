04c93347d4ddaba539be8e829900af8c9cceb4cc
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 12:26:36 +0100

    Update WebHead.class.php

--- CLASS/WebHead.class.php
+++ CLASS/WebHead.class.php
@@ -8,7 +8,7 @@
 				case "IndexHead":
 					echo 
 						'<head>
-							<title>IEPA</title>
+							<title>PACANOSIU</title>
 							<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 							<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 							<link rel="stylesheet" href="css/1920x1080style.css">
