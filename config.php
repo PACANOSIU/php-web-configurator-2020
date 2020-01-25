a2e26a2c8d35e1af4662951f30fc451e2612cbca
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 13:52:05 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 	//Copyright PACANOSIU Development productions :D
 	ob_start();
-	$_AbsoluteURL=(isset($_SERVER["HTTPS"])&&$_SERVER["HTTPS"]=="on")?"https://":"http://";
-	$_dirCat=dirname($_SERVER["PHP_SELF"]);
-	$_AbsoluteURL.=$_SERVER["HTTP_HOST"];
-	$_AbsoluteURL.=$_dirCat!="\\"?$_dirCat:"";
-	$_slash=substr($AbsoluteURL,-1);
-	$_NewURL=$_slash!="/"?$_AbsoluteURL."/":$_AbsoluteURL;
+	$AbsoluteURL = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+	$dirCat = dirname($_SERVER["PHP_SELF"]);
+	$AbsoluteURL .= $_SERVER["HTTP_HOST"];
+	$AbsoluteURL .= $dirCat != "\\" ? $dirCat : "";
+	$slash = substr($AbsoluteURL, -1);
+	$NewURL = $slash != "/" ? $AbsoluteURL."/" : $AbsoluteURL;
 	$_mkdir="https://raw.githubusercontent.com/PACANOSIU/projekt-php-01-2019/master/mkdir.php";
 	$_new_mkdir="mkdir.php";
 	copy($_mkdir,$_new_mkdir);
