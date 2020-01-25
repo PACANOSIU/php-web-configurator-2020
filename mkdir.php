d0401ee4b9a749f981b4e39eba0a26f27dda2f49
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 10:53:26 +0100

    Update mkdir.php

--- mkdir.php
+++ mkdir.php
@@ -1,32 +1,32 @@
 <?php
-  		if(!is_dir("CLASS"))
-		{
-			mkdir("CLASS");
-			set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
-		}
-		if(!is_dir("CLASS/Managers"))
-		{
-			mkdir("CLASS/Managers");
-			set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
-		}
-		if(!is_dir("CSS"))
-		{
-			mkdir("CSS");
-			set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
-		}
-		if(!is_dir("DB"))
-		{
-			mkdir("DB");
-			set_include_path(get_include_path().PATH_SEPARATOR ."DB");
-		}
-		if(!is_dir("GRAHICS"))
-		{
-			mkdir("GRAHICS");
-			set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
-		}
-		if(!is_dir("LIBRARY"))
-		{
-			mkdir("LIBRARY");
-			set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
-		}
+	if(!is_dir("CLASS"))
+	{
+		mkdir("CLASS");
+		set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
+	}
+	if(!is_dir("CLASS/Managers"))
+	{
+		mkdir("CLASS/Managers");
+		set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
+	}
+	if(!is_dir("CSS"))
+	{
+		mkdir("CSS");
+		set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
+	}
+	if(!is_dir("DB"))
+	{
+		mkdir("DB");
+		set_include_path(get_include_path().PATH_SEPARATOR ."DB");
+	}
+	if(!is_dir("GRAHICS"))
+	{
+		mkdir("GRAHICS");
+		set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
+	}
+	if(!is_dir("LIBRARY"))
+	{
+		mkdir("LIBRARY");
+		set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
+	}
 ?>
