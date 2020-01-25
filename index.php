10b9cb02dc25151dcbe08dbe335b14d5255e7650
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 10:50:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,36 @@
 	//Copyright PACANOSIU Development productions :D
 	if($_GET)
 	{
+		if(!is_dir("CLASS"))
+		{
+			mkdir("CLASS");
+			set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
+		}
+		if(!is_dir("CLASS/Managers"))
+		{
+			mkdir("CLASS/Managers");
+			set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
+		}
+		if(!is_dir("CSS"))
+		{
+			mkdir("CSS");
+			set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
+		}
+		if(!is_dir("DB"))
+		{
+			mkdir("DB");
+			set_include_path(get_include_path().PATH_SEPARATOR ."DB");
+		}
+		if(!is_dir("GRAHICS"))
+		{
+			mkdir("GRAHICS");
+			set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
+		}
+		if(!is_dir("LIBRARY"))
+		{
+			mkdir("LIBRARY");
+			set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
+		}
 		$_konfigurator="https://raw.githubusercontent.com/PACANOSIU/projekt-php-01-2019/master/configurator.php";
 		$_new_konfigurator="configurator.php";
 		copy($_konfigurator,$_new_konfigurator);
