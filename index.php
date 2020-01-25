bcbcdb6f9a2eb257f4f9c0ad51a129e09bf9d67a
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 11:34:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	//Copyright PACANOSIU Development productions :D
 	if($_GET)
 	{
+		session_start();
 		$_konfigurator="https://raw.githubusercontent.com/PACANOSIU/projekt-php-01-2019/master/configurator.php";
 		$_new_konfigurator="configurator.php";
 		copy($_konfigurator,$_new_konfigurator);
@@ -11,32 +12,36 @@
 		$_config="https://raw.githubusercontent.com/PACANOSIU/projekt-php-01-2019/master/config.php";
 		$_new_config="config.php";
 		copy($_config,$_new_config);
+		require_once("config.php");
 		header("location:configurator.php");
-	}	
-	echo
-		'<!DOCTYPE html>
-		<html>
-			<head>
-				<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-			</head>
-			<body>
-				<div style="text-align:center;font-family:Montserrat,sans-serif;">
-					<div>
-						Welcome to the "PACANOSIU" website configurator
-						<br />
-						Thank you for your interest in my product.
-						<br />
-						To be up to date <a href="">SUBSCRIBE</a>.
-						<br />
-						<form method="GET">
-							<input style="font-family:Montserrat,sans-serif;font-size:20px;padding:10px;margin:10px;cursor:pointer;" type="submit" name="konfigurator" value="START CONFIGURING YOUR WEBSITE">
-						</form>
+	}
+	else
+	{
+		echo
+			'<!DOCTYPE html>
+			<html>
+				<head>
+					<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+				</head>
+				<body>
+					<div style="text-align:center;font-family:Montserrat,sans-serif;">
+						<div>
+							Welcome to the "PACANOSIU" website configurator
+							<br />
+							Thank you for your interest in my product.
+							<br />
+							To be up to date <a href="">SUBSCRIBE</a>.
+							<br />
+							<form method="GET">
+								<input style="font-family:Montserrat,sans-serif;font-size:20px;padding:10px;margin:10px;cursor:pointer;" type="submit" name="konfigurator" value="START CONFIGURING YOUR WEBSITE">
+							</form>
+						</div>
 					</div>
-				</div>
-			</body>
-			<footer style="position:absolute;bottom:1px;right:1px;font-size:10px;font-family:Montserrat,sans-serif;">
-				Configurator PACANOSIU BETA 2020.01
-			</footer>
-		</html>'
-	;
+				</body>
+				<footer style="position:absolute;bottom:1px;right:1px;font-size:10px;font-family:Montserrat,sans-serif;">
+					Configurator PACANOSIU BETA 2020.01
+				</footer>
+			</html>'
+		;
+	}
 ?>
