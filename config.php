d5cfe8bd8468e0ea998083a427112e6323be2f0d
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 08:55:07 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 	ob_start();
-	$_AbsoluteURL = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-	$_dirCat = dirname($_SERVER["PHP_SELF"]);
+	$_AbsoluteURL=(isset($_SERVER["HTTPS"])&&$_SERVER["HTTPS"]=="on")?"https://":"http://";
+	$_dirCat=dirname($_SERVER["PHP_SELF"]);
 	$_AbsoluteURL .= $_SERVER["HTTP_HOST"];
-	$_AbsoluteURL .= $_dirCat != "\\" ? $_dirCat : "";
-	$_slash = substr($AbsoluteURL, -1);
-	$_NewURL = $_slash != "/" ? $_AbsoluteURL."/" : $_AbsoluteURL;
+	$_AbsoluteURL .= $_dirCat!= "\\"?$_dirCat:"";
+	$_slash=substr($AbsoluteURL, -1);
+	$_NewURL=$_slash!= "/" ? $_AbsoluteURL."/":$_AbsoluteURL;
 	spl_autoload_register(function($_className)
 	{
 		include_once($_className.".class.php");
