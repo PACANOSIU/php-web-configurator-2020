55992777b1f1a98d5750eb799e4b5be4620cfd6b
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 12:00:20 +0100

    Create ExtensionController.class.php

---
+++ CLASS/Managers/ExtensionController.class.php
@@ -0,0 +1,16 @@
+<?php
+	class ExtensionController 
+	{   
+		private $active_page;    
+		public function __construct($ACTIVE_PAGE) 
+		{        
+			$this->active_page = $ACTIVE_PAGE;        
+			switch($this->active_page) 
+			{            
+				case "welcome-website":
+				require_once $this->active_page.".php";
+				break;
+			}        
+		}   
+	}
+?>
