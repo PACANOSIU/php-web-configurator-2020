d2b8a4ac460bb477c791eca9e98d4ad1d2ca2df4
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 13:03:21 +0100

    Create WebReklamaHeader.class.php

---
+++ CLASS/WebReklamaHeader.class.php
@@ -0,0 +1,28 @@
+<?php
+	class WebReklamaHeader 
+	{
+		static public function load($MODULE) 
+		{
+			switch($MODULE) 
+			{		
+				case "IndexReklamaHeader":
+					echo 
+						'<section class="web_bars" align="center" title="REKLAMA">
+							<div style="width:730px;height:100px;display:inline-block;margin:-1px 0 -13px 0;">
+								
+							</div>
+							<div style="display:inline-block;color:rgb(222,222,222);vertical-align:top;margin:30px 20px 0 20px;font-family: Montserrat, sans-serif;font-size:12px;">
+								<- ADVERTISE ->
+							</div>
+							<div style="width:730px;height:100px;display:inline-block;margin:-1px 0 -13px 0;">
+								
+							</div>
+						</section>'
+					;
+				break;
+				default;
+				break;
+			}
+		}
+	}
+?>
