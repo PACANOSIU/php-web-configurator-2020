8d1fdfcccc21bf63f88a6b04fb5077cbc61b4e7d
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 14:17:51 +0100

    Update mkdir.php

--- mkdir.php
+++ mkdir.php
@@ -1,33 +1,32 @@
 <?php
-	ob_start();
 	if(!is_dir("CLASS"))
 	{
 		mkdir("CLASS");
-		set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
 	}
 	if(!is_dir("CLASS/Managers"))
 	{
 		mkdir("CLASS/Managers");
-		set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
 	}
 	if(!is_dir("CSS"))
 	{
 		mkdir("CSS");
-		set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
 	}
 	if(!is_dir("DB"))
 	{
 		mkdir("DB");
-		set_include_path(get_include_path().PATH_SEPARATOR ."DB");
 	}
 	if(!is_dir("GRAHICS"))
 	{
 		mkdir("GRAHICS");
-		set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
 	}
 	if(!is_dir("LIBRARY"))
 	{
 		mkdir("LIBRARY");
-		set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
 	}
+	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
+	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
+	set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
+	set_include_path(get_include_path().PATH_SEPARATOR ."DB");
+	set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
+	set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
 ?>
