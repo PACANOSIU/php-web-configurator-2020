e13d786104edcde1da9d759f15377cc47ffe8682
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 12:53:00 +0100

    Update WebIndex.class.php

--- CLASS/WebIndex.class.php
+++ CLASS/WebIndex.class.php
@@ -5,28 +5,25 @@
 		{
 			switch($MODULE) 
 			{		
-				case"IndexIndex":
-					if($_SERVER["REMOTE_ADDR"]=="::1")
-					{		
-						echo
-							'<!DOCTYPE html>
-							<html>'
-						;
-						WebHead::load("IndexHead");
-						echo
-							'<body align="center">'
-						;
-						WebHeader::load("IndexHeader");
-						WebReklamaHeader::load("IndexReklamaHeader");
-						WebBars::load("IndexBars");
-						WebWritePole::load("IndexWritePole");
-						WebLegs::load("IndexLegs");
-						WebFooter::load("IndexFooter");
-						echo
-							'</body>
-							</html>'
-						;
-					}				
+				case"IndexIndex":		
+					echo
+						'<!DOCTYPE html>
+						<html>'
+					;
+					WebHead::load("IndexHead");
+					echo
+						'<body align="center">'
+					;
+					WebHeader::load("IndexHeader");
+					WebReklamaHeader::load("IndexReklamaHeader");
+					WebBars::load("IndexBars");
+					WebWritePole::load("IndexWritePole");
+					WebLegs::load("IndexLegs");
+					WebFooter::load("IndexFooter");
+					echo
+						'</body>
+						</html>'
+					;				
 				break;
 				default;
 				break;
