e233d19c9e6044bbd4d50ff72990f36082a38020
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 08:53:34 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-	$db_host = "root";
-	$db_user_name = "";
-	$db_user_password = "";
-	$db_name = "";
-	$db_connect = @new mysqli($db_host, $db_user_name, $db_user_password, $db_name);
+	$db_host="root";
+	$db_user_name="";
+	$db_user_password="";
+	$db_name="";
+	$db_connect=@new mysqli($db_host,$db_user_name,$db_user_password,$db_name);
 ?>
