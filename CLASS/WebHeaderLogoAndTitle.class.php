417712bb9a9e9dc2131564adcd6cf6431766b2ee
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-25 19:46:10 +0100

    Create WebHeaderLogoAndTitle.class.php

---
+++ CLASS/WebHeaderLogoAndTitle.class.php
@@ -0,0 +1,25 @@
+<?php
+	class WebHeaderLogoAndTitle 
+	{
+		static public function load($MODULE) 
+		{
+			switch($MODULE) 
+			{		
+				case "IndexDisplay":
+					echo 
+						'<div class="header_logo_and_title">
+								<a href="/">
+									<img src="" style="height:40px;">
+									<h1>
+										SITE NAME
+									</h1>
+								</a>
+							</div>'
+					;
+				break;
+				default;
+				break;
+			}
+		}
+	}
+?>
