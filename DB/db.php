5f4c14d6a587be9075684645839f143c0ce17bcd
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-26 16:40:28 +0100

    Update db.php

--- DB/db.php
+++ DB/db.php
@@ -1,8 +1,7 @@
 <?php
 	//Copyright PACANOSIU Development productions :D
-	$db_host="root";
-	$db_user_name="";
-	$db_user_password="";
+	require_once"db_users.php";
+	$db_host="localhost";
 	$db_name="";
 	$db_connect=@new mysqli($db_host,$db_user_name,$db_user_password,$db_name);
 ?>
