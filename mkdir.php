41439e6b0ab408942cf1e47946b0fcb91359e28f
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-01-26 13:51:52 +0100

    Update mkdir.php

--- mkdir.php
+++ mkdir.php
@@ -23,10 +23,15 @@
 	{
 		mkdir("LIBRARY");
 	}
+	if(!is_dir("USERS"))
+	{
+		mkdir("USERS");
+	}
 	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
 	set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."DB");
 	set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
+	set_include_path(get_include_path().PATH_SEPARATOR ."USERS");
 ?>
