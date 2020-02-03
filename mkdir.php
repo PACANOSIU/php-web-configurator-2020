ccbf4a3104d97609131d6e227de38d17f5da1043
PACANOSIU <60273697+PACANOSIU@users.noreply.github.com>
2020-02-03 20:57:27 +0100

    Update mkdir.php

--- mkdir.php
+++ mkdir.php
@@ -7,6 +7,10 @@
 	{
 		mkdir("CLASS/Managers");
 	}
+	if(!is_dir("CONFIGURATORS"))
+	{
+		mkdir("CONFIGURATORS");
+	}
 	if(!is_dir("CSS"))
 	{
 		mkdir("CSS");
@@ -15,9 +19,25 @@
 	{
 		mkdir("DB");
 	}
+	if(!is_dir("DB/Users"))
+	{
+		mkdir("DB/Users");
+	}	
+	if(!is_dir("DB/Connections"))
+	{
+		mkdir("DB/Connections");
+	}
+	if(!is_dir("FORMS"))
+	{
+		mkdir("FORMS");
+	}
 	if(!is_dir("GRAHICS"))
 	{
 		mkdir("GRAHICS");
+	}
+	if(!is_dir("HOSTS"))
+	{
+		mkdir("HOSTS");
 	}
 	if(!is_dir("LIBRARY"))
 	{
@@ -29,9 +49,15 @@
 	}
 	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."CLASS/Managers");
+	set_include_path(get_include_path().PATH_SEPARATOR ."CONFIGURATORS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."CSS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."DB");
+	set_include_path(get_include_path().PATH_SEPARATOR ."DB/Users");	
+	set_include_path(get_include_path().PATH_SEPARATOR ."DB/Connections");
+	set_include_path(get_include_path().PATH_SEPARATOR ."FORMS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."GRAHICS");
+	set_include_path(get_include_path().PATH_SEPARATOR ."HOSTS");
 	set_include_path(get_include_path().PATH_SEPARATOR ."LIBRARY");
 	set_include_path(get_include_path().PATH_SEPARATOR ."USERS");
 ?>
+
